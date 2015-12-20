010b53972f4e68137317bb1b8e897785775d826b
Happy Du <Happy Du>
2015-12-20 23:32:33 +0800

    与android端相连接，发送数据

--- app.js
+++ app.js
@@ -3,99 +3,12 @@ var port = process.env.PORT || 3000
 var path = require('path')
 var app =express()
 var bodyParser=require('body-parser')
-var DataBase=require('./public/lib/js/ControlData.js') 
 
 app.set('views','./views/pages')
 app.set('view engine','jade')
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(express.static(path.join(__dirname,'public')))
+require('./config/router')(app)
 app.listen(port)
 console.log('NFCWeb start on port:'+port)
-
-//设置路由分发
-//首页
-app.get('/',function(request,reponse){
-	reponse.render('index',{
-		title: 'NFCMarket'
-	})
-})
-
-//商品总的列表
-app.get('/list',function(request,reponse){
-    var data = new DataBase();
-    var query = "SELECT numbers,price,updatetime,id,name " +
-              "FROM wholeinfo ";
-	data.selectFromWhole(query,5,function(goods){
-	     reponse.render('list',{
-		 title: '商品列表',
-		 goods
-	})
-	});
-})
-
-//添加新的商品
-app.get('/add',function(request,reponse){
-	reponse.render('add',{
-		title: '添加新的商品',
-		good: {
-				id: '',
-				name: '',
-				productplace: '',
-				energy: '',
-				price: '',
-				time: '',
-				pic: '',
-				style:''
-			}
-	})
-})
-
-//更新商品信息
-app.get('/update/:id',function(request,reponse){
-	var id = request.params.id;
-//	console.log("id is "+id);
-	var data = new DataBase();
-    var query = "SELECT numbers,price,updatetime,id,name,style,productplace,energy " +
-              "FROM wholeinfo where id = '"+id+"'";
-	data.selectFromWhole(query,8,function(goods){ 
-		 var good = goods[0];
-	     reponse.render('add',{
-		 title: '更新商品的信息',
-		 good
-		 });
-	});
-})
-
-//删除商品
-app.delete('/list',function(request,reponse){
-	var id = request.query.id;
-// 	console.log("receive id is "+id);
-	var data = new DataBase();
-	var sdelete="delete from wholeinfo where id = '"+id+"'";
-	data.deleteinfo(sdelete,function(){
-		  reponse.json({success:1});
-	});
-})
-//查看具体销售信息
-app.get('/detail/:id',function(request,reponse){
-	var id = request.params.id;
-//	console.log("id is "+id);
-	var data = new DataBase();
-    var query = "SELECT numbers,price,updatetime,id,name,style,productplace,energy " +
-              "FROM wholeinfo where id = '"+id+"'";
-	data.selectFromWhole(query,8,function(goods){ 
-		 var good = goods[0];
-		 var query = "SELECT numbers,price,saletime " +
-              "FROM detailinfo where id = '"+id+"'";
-		 data.selectFromWhole(query,3,function(details){ 
-              //	 good.setpic((id+".jpg"));
-	//	 console.log("place is "+good.productplace+" updatetime is "+good.updatetime+" style is "+good.style+" pic is "+good.pic);
-	     reponse.render('detail',{
-		 title: '商品的详细信息',
-		 good,
-		 details
-		 });
-	});
-	});
-})
 
---
+++ config/router.js
@@ -0,0 +1,205 @@
+//设置路由分发
+var DataBase=require('../public/libs/js/ControlData.js') 
+var fs = require('fs')
+var path = require('path')
+//首页
+module.exports = function(app){
+		app.get('/',function(request,reponse){
+			reponse.render('index',{
+				title: 'NFCMarket'
+			})
+		})
+
+		//商品总的列表
+		app.get('/list',function(request,reponse){
+		    var data = new DataBase();
+		    var query = "SELECT numbers,price,updatetime,id,name " +
+		              "FROM wholeinfo ";
+			data.selectFromWhole(query,5,function(goods){
+				 for(var i = 0 ;i<goods.length;i++){
+				 	goods[i].updatetime = getLocalTime(goods[i].updatetime);
+				 }
+			     reponse.render('list',{
+				 title: '商品列表',
+				 goods
+			})
+			});
+		})
+
+		//添加新的商品
+		app.get('/add',function(request,reponse){
+			var action = "/add"
+			reponse.render('add',{
+				title: '添加新的商品',
+				good: {
+						id: '',
+						name: '',
+						productplace: '',
+						energy: '',
+						price: '',
+						time: '',
+						pic: '',
+						style:''
+					},
+				action
+			})
+		})
+
+		//更新商品信息
+		app.get('/update/:id',function(request,reponse){
+			var id = request.params.id;
+		//	console.log("id is "+id);
+			var data = new DataBase();
+		    var query = "SELECT numbers,price,updatetime,id,name,style,productplace,energy " +
+		              "FROM wholeinfo where id = '"+id+"'";
+		    var action ="/update"
+			data.selectFromWhole(query,8,function(goods){ 
+				 var good = goods[0];
+			     reponse.render('add',{
+				 title: '更新商品的信息',
+				 good,
+		         action
+				 });
+			});
+		})
+
+		//删除商品
+		app.delete('/list',function(request,reponse){
+			var id = request.query.id;
+		// 	console.log("receive id is "+id);
+			var data = new DataBase();
+			var sdelete="delete from wholeinfo where id = '"+id+"'";
+			data.Changeinfo(sdelete,true,function(){
+				  reponse.json({success:1});
+			});
+		})
+		//查看具体销售信息
+		app.get('/detail/:id',function(request,reponse){
+			var id = request.params.id;
+		//	console.log("id is "+id);
+			var data = new DataBase();
+		    var query = "SELECT numbers,price,updatetime,id,name,style,productplace,energy " +
+		              "FROM wholeinfo where id = '"+id+"'";
+			data.selectFromWhole(query,8,function(goods){ 
+				 var good = goods[0];
+				 good.updatetime = getLocalTime(good.updatetime);
+				 var query = "SELECT numbers,price,saletime " +
+		              "FROM detailinfo where id = '"+id+"'";
+				 data.selectFromWhole(query,3,function(details){ 
+		              //	 good.setpic((id+".jpg"));
+		         for(var i = 0 ; i < details.length ; i++){
+                     details[i].updatetime = getLocalTime(details[i].updatetime);
+		         }
+			//	 console.log("place is "+good.productplace+" updatetime is "+good.updatetime+" style is "+good.style+" pic is "+good.pic);
+			     reponse.render('detail',{
+				 title: '商品的详细信息',
+				 good,
+				 details
+				 });
+			   });
+			});
+		})
+
+		//更新产品信息
+		app.post('/update',function(request,response){
+			var id = request.body.id;
+			console.log("id is "+id);
+			var name = request.body.name;
+			var style = request.body.style;
+			var productplace = request.body.productplace;
+			var price = request.body.price;
+			var energy = request.body.energy;
+			uploadFile(id,request,function(){
+				var updatetime = Date.parse(new Date())/1000;
+				var data = new DataBase();
+				// console.log("update");
+				var query = "SELECT numbers " +
+			              "FROM wholeinfo where id = '"+id+"'";
+				data.selectFromWhole(query,1,function(goods){ 
+			         var number = goods[0].numbers;
+			         var sdelete="delete from wholeinfo where id = '"+id+"'";
+					   data.Changeinfo(sdelete,true,function(){
+							var sinsert="insert into wholeinfo (id,name,style,productplace,"+
+				           "price,energy,numbers,updatetime) values ('"+id+"','"+name+"','"+style+
+				           "','"+productplace+"','"+price+"','"+energy+"',"+number+",'"+updatetime+"')"
+			                  data.Changeinfo(sinsert,true,function(){
+			                                  response.redirect('/detail/'+id);
+					     });
+				    });
+				});
+			});
+		})
+
+		//添加产品信息
+		app.post('/add',function(request,response){
+			var id = request.body.id;
+			var name = request.body.name;
+			var style = request.body.style;
+			var productplace = request.body.productplace;
+			var price = request.body.price;
+			var energy = request.body.energy;
+			var updatetime = Date.parse(new Date())/1000;
+			var data = new DataBase();
+			var sinsert="insert into wholeinfo (id,name,style,productplace,"+
+			           "price,energy,numbers,updatetime) values ('"+id+"','"+name+"','"+style+
+			           "','"+productplace+"','"+price+"','"+energy+"',"+0+",'"+updatetime+"')"
+			  // console.log("add exec is "+ sinsert);
+		      data.Changeinfo(sinsert,true,function(){
+		                      response.redirect('/list');
+			});
+		})
+
+		//Android端获取产品信息
+		app.post('/android/updateinfo',function(request,response){
+            var updatetime = request.body.updatetime;
+             // console.log("updatetime is "+ updatetime);
+			var data = new DataBase();
+		    var query = "SELECT numbers,price,updatetime,id,name " +
+		              "FROM wholeinfo where updatetime > "+updatetime;
+			data.selectFromWhole(query,5,function(goods){
+			     // console.log("good is "+ goods);
+			      response.json(goods);
+			})
+
+		})
+}
+
+//把时间戳改为正常的格式
+function getLocalTime(nS) {     
+       return new Date(parseInt(nS) * 1000).toLocaleString().substr(0,16).replace(/年|月/g, "/").replace(/日/g, " ");      
+    } 
+
+function uploadFile(id,req,callback){
+	var fs = require('fs');
+    var formidable = require("formidable");
+    var form = new formidable.IncomingForm();
+    form.uploadDir = "./public/";//改变临时目录
+    form.parse(req, function(error, fields, files){
+        for(var key in files){
+            var file = files[key];
+            switch (file.type){
+                case "image/jpeg":
+                    fName = id + ".jpg";
+                    break;
+                case "image/png":
+                    fName = id + ".png";
+                    break;
+                default :
+                    fName = id + ".png";
+                    break;
+            }
+            console.log(file.size);
+            var uploadDir = "./public/image/" + fName;
+            fs.rename(file.path, uploadDir, function(err) {
+                if (err) {
+                   console.log(err);
+                   return;
+                }
+                // res.write("upload image:<br/>");
+                // res.write("<img src='/imgShow?id=" + fName + "' />");
+                // res.end();
+                callback();
+            });
+        }
+    });
+}
